52136c2c8a6e2ab305c557c27634820b3f599e9b
hamidhosen42 <mdhamidhosen8444@gmail.com>
2023-05-01 09:16:47 +0600

    Array and Tuples in typescript

---
+++ Mission 1 - Be A Typescript Technocrat/Module 2 - Explore Basic Types of Typescript/dist/array.js
@@ -0,0 +1,19 @@
+"use strict";
+const names = ['Apple', 'Orange', 'Banana'];
+const rollNumber = [12, 34, 34];
+rollNumber[8] = 345;
+// rollNumber[9]="sfds"
+rollNumber.map(roll => console.log(roll));
+names.map(name => console.log(name));
+const names1 = ['Apple', 'Orange', 'Banana', 23, 34, true];
+names1.push("Hamid");
+console.log(names1);
+// tuple
+const names2 = ['Hamid', 23];
+names2[0] = "Hosen";
+names2[1] = "Azad";
+console.log(names2);
+const names3 = [23, "Asif"];
+names3[0] = 50;
+names3[1] = "Mikdad";
+console.log(names3);
