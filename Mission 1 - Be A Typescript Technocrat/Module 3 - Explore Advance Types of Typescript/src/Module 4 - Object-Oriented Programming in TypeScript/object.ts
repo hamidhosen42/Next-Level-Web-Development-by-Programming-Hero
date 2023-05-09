530f732e2c92b2491b68c48f405119faf58e4607
hamidhosen42 <mdhamidhosen8444@gmail.com>
2023-05-09 06:06:37 +0600

    How to Create Class, Object , Parameter Properties

--- Mission 1 - Be A Typescript Technocrat/Module 3 - Explore Advance Types of Typescript/src/Module 4 - Object-Oriented Programming in TypeScript/object.ts
+++ Mission 1 - Be A Typescript Technocrat/Module 3 - Explore Advance Types of Typescript/src/Module 4 - Object-Oriented Programming in TypeScript/object.ts
@@ -1,13 +1,13 @@
 class Animal{
     // public name:string;
-    public species:string;
-    public sound:string;
+    // public species:string;
+    // public sound:string;
 
     // Parameter Properties
-    constructor(public name:string, species:string,sound:string){
+    constructor(public name:string, public species:string,public sound:string){
         // this.name=name,
-        this.species = species,
-        this.sound= sound
+        // this.species = species,
+        // this.sound= sound
     }
     makeSound(){
         console.log(`This ${this.name} says ${this.sound}`);
@@ -17,3 +17,4 @@ class Animal{
 const dog = new Animal("German Shepard","Dog","Ghew Ghew");
 const dog1 = new Animal("German Shepard","Dog","Ghew Ghew");
 dog.makeSound();
+dog1.makeSound();
