bef91e268f8cc5bf6bc307b8f991d78f4c6468b9
hamidhosen42 <mdhamidhosen8444@gmail.com>
2023-05-12 23:23:52 +0600

    How to organize your structure , MVC vs Modular

--- Mission 2 - Be A Mongoos Master/Module 6 - Installation and Explore Read and Write Queries/practice-mongoose/src/app.ts
+++ Mission 2 - Be A Mongoos Master/Module 6 - Installation and Explore Read and Write Queries/practice-mongoose/src/app.ts
@@ -23,85 +23,6 @@ app.get('/', (req: Request, res: Response, next: NextFunction) => {
     Step - 4 : Database Quary
     */
 
-    // 1. Create an interface representing a document in MongoDB.
-    interface IUser {
-        id: string;
-        role: "student";
-        password: string;
-        name: {
-            firstName: string;
-            middleName?: string;
-            lastName: string;
-        };
-        dateOfBirth?: string;
-        gender: "male" | "female";
-        email?: string;
-        contactNo: string;
-        emergencyContactNo: string;
-        presentAddress: string;
-        permanentAddress: string;
-    };
-
-    // 2. Create a Schema corresponding to the document interface.
-    const userSchema = new Schema<IUser>({
-        id: {
-            type: String,
-            required: true,
-            unique: true,
-        },
-        role: {
-            type: String,
-            required: true,
-        },
-        password: {
-            type: String,
-            required: true,
-        },
-
-        name: {
-            firstName: {
-                type: String,
-                required: true,
-            },
-            middleName: {
-                type: String,
-            },
-            lastName: {
-                type: String,
-                required: true,
-            },
-        },
-        dateOfBirth: {
-            type: String,
-        },
-        gender: {
-            type: String,
-            enum: ["male", "female"],
-        },
-        email: {
-            type: String,
-        },
-        contactNo: {
-            type: String,
-            required: true,
-        },
-        emergencyContactNo: {
-            type: String,
-            required: true,
-        },
-        presentAddress: {
-            type: String,
-            required: true,
-        },
-        permanentAddress: {
-            type: String,
-            required: true,
-        },
-    });
-
-    // 3. Create a Model.
-    const User = model<IUser>('User', userSchema);
-
     // 4. Connect to MongoDB
     const createUserToDB = async () => {
         const user = new User({
@@ -128,3 +49,14 @@ app.get('/', (req: Request, res: Response, next: NextFunction) => {
 });
 
 export default app;
+
+// Parrern MVC, Modular
+
+/*
+Interface interface.ts
+Schema , Model -> model.ts
+
+route
+route function -> controller.ts
+Database Query Function --> service
+*/
