15fd2eb018e31a36d3ad195b7a06a51c8be1acb1
hamidhosen42 <mdhamidhosen8444@gmail.com>
2023-05-12 14:16:18 +0600

    What is module ,different formats of module technique, import and export

---
+++ Mission 1 - Be A Typescript Technocrat/Module 5.5 - Bonus Module/dist/main.js
@@ -0,0 +1,6 @@
+import { add as addTwo, sub } from "./other";
+const add = (param1, param2, param3) => {
+    return param1 + param2 + param3;
+};
+const res = addTwo(4, 6);
+const res1 = sub(5, 4);
---
+++ Mission 1 - Be A Typescript Technocrat/Module 5.5 - Bonus Module/src/main.ts
@@ -0,0 +1,9 @@
+import { add as addTwo,sub} from "./other";
+
+const add=(param1:number,param2:number,param3:number):number=>{
+    return param1+param2+param3;
+};
+
+const res = addTwo(4,6);
+
+const res1 = sub(5,4);
