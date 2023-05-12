eb3f15f225cf27f2e8d9c599163b8ab9b5befa3b
hamidhosen42 <mdhamidhosen8444@gmail.com>
2023-05-12 07:52:59 +0600

    Encapsulation and module Summary

--- Mission 1 - Be A Typescript Technocrat/Module 4 - Object-Oriented Programming in TypeScript/src/abstraction.ts
+++ Mission 1 - Be A Typescript Technocrat/Module 4 - Object-Oriented Programming in TypeScript/src/abstraction.ts
@@ -1,64 +1,64 @@
-//!interface
+// //!interface
+
+// // interface IVehicle {
+// //   name: string;
+// //   model: string;
+// // }
+
+// // const vehicle: IVehicle = {
+// //   name: "Car",
+// //   model: "2000",
+// // };
 
 // interface IVehicle {
-//   name: string;
-//   model: string;
+//     startEngine(): void;
+//     stopEngine(): void;
+//     move?(): void;
 // }
 
-// const vehicle: IVehicle = {
-//   name: "Car",
-//   model: "2000",
-// };
+// class Vehicles implements IVehicle {
+//     constructor(
+//         public name: string,
+//         public brand: string,
+//         public model: number
+//     ) { }
+//     startEngine(): void {
+//         console.log(" I am starting engine... ");
+//     }
+//     stopEngine(): void {
+//         console.log(" I am syopping engine");
+//     }
+//     move(): void {
+//         console.log(" I am syopping engine");
+//     }
+// }
 
-interface IVehicle {
-    startEngine(): void;
-    stopEngine(): void;
-    move?(): void;
-}
+// const vehicle1 = new Vehicles("Car", "Toyota", 2000);
 
-class Vehicles implements IVehicle {
-    constructor(
-        public name: string,
-        public brand: string,
-        public model: number
-    ) { }
-    startEngine(): void {
-        console.log(" I am starting engine... ");
-    }
-    stopEngine(): void {
-        console.log(" I am syopping engine");
-    }
-    move(): void {
-        console.log(" I am syopping engine");
-    }
-}
+// //!abstract class
 
-const vehicle1 = new Vehicles("Car", "Toyota", 2000);
+// abstract class Vehicle {
+//     constructor(
+//         public name: string,
+//         public brand: string,
+//         public model: number
+//     ) { }
+//     abstract startEngine(): void
+//     abstract stopEngine(): void
+//     move(): void {
+//         console.log(" I am syopping engine");
+//     }
 
-//!abstract class
+// }
 
-abstract class Vehicle {
-    constructor(
-        public name: string,
-        public brand: string,
-        public model: number
-    ) { }
-    abstract startEngine(): void
-    abstract stopEngine(): void
-    move(): void {
-        console.log(" I am syopping engine");
-    }
+// class Car extends Vehicle {
+//     startEngine(): void {
+//         console.log(" I am starting engine... ");
+//     }
+//     stopEngine(): void {
+//         console.log(" I am stopping engine... ");
+//     }
 
-}
+// }
 
-class Car extends Vehicle {
-    startEngine(): void {
-        console.log(" I am starting engine... ");
-    }
-    stopEngine(): void {
-        console.log(" I am stopping engine... ");
-    }
-
-}
-
-//   const car1= new Vehicle('Car','Honda',2015)
+// //   const car1= new Vehicle('Car','Honda',2015)
