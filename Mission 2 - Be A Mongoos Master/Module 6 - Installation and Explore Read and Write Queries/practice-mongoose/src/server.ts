3cd3eeb7e790b74f8ffbf83ba0e4ce554d14769b
hamidhosen42 <mdhamidhosen8444@gmail.com>
2023-05-12 20:42:58 +0600

    How to organize your code alittle more . install and implement types

--- Mission 2 - Be A Mongoos Master/Module 6 - Installation and Explore Read and Write Queries/practice-mongoose/src/server.ts
+++ Mission 2 - Be A Mongoos Master/Module 6 - Installation and Explore Read and Write Queries/practice-mongoose/src/server.ts
@@ -1,23 +1,19 @@
-const express = require('express')
-const mongoose = require('mongoose');
-const app = express()
-const port = 5000
+import mongoose from 'mongoose';
+import app from './app';
+
+const port:number = 5000
 
 // DataBase Connection
 async function pracice() {
     try {
         await mongoose.connect('mongodb://127.0.0.1:27017/practice-mongoose');
         console.log(`Database connection successful`);
+
+        app.listen(port, () => {
+            console.log(`Server s listening on port ${port}`)
+        })
     } catch (err) {
         console.log(`Failed to connect database`, err);
     }
-}
+};
 pracice()
-
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-
-app.listen(port, () => {
-    console.log(`Server s listening on port ${port}`)
-})
